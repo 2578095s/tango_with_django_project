50b8186abfc3f518221c0eab536850e59dbce024
“Rhythm <2578095s@student.gla.ac.uk>
2022-02-10 17:21:36 +0000

    chapter 5

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 
 from django.shortcuts import HttpResponse
 
+from rango.models import Category
+
+from rango.models import Page
+
 
 def index(request):
-  context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+  category_list = Category.objects.order_by('-likes')[:5]
+  
+  context_dict = {}
+  context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+  context_dict['categories'] = category_list
   return render(request, 'rango/index.html', context=context_dict)
 
+
+
 def about(request):
-  context_dict = {'author': 'Rhythm Sodhi'}
+  context_dict = {'name': 'Rhythm Sodhi'}
   return render(request, 'rango/about.html', context = context_dict)
+
+
+def show_category(request, category_name_slug):
+  try:
+    context_dict['pages'] = pages
+  except Category.DoesNotExist:
+    context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
 
